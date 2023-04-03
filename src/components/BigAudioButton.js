fe96fae17b9edde0ded3a892e2f5167dad45e18f
pahancha <pahanchathuranga123@gmail.com>
2023-04-03 15:18:35 +0530

    main button adjustments

--- src/components/BigAudioButton.js
+++ src/components/BigAudioButton.js
@@ -10,13 +10,15 @@ const BigAudioButton = ({ onPress }) => {
   };
 
   const screenWidth = Dimensions.get('window').width;
-  const buttonSize = screenWidth * 0.8;
+  const screenHeight = Dimensions.get('window').height;
+  const buttonWidth= screenWidth * 0.8;
+  const buttonHeight= screenHeight * 0.5;
   const buttonColor = isRecording ? '#404040' : '#000000';
 //   const buttonColor = isRecording ? '#115931' : '#000000';
 
 
   return (
-    <TouchableOpacity style={[styles.button, { width: buttonSize, height: buttonSize, backgroundColor: buttonColor }]} onPress={handlePress}>
+    <TouchableOpacity style={[styles.button, { width: buttonWidth, height: buttonHeight, backgroundColor: buttonColor }]} onPress={handlePress}>
       <Text style={styles.title}>
         {isRecording ? 'Press to stop recording' : 'Press to talk'}
       </Text>
@@ -26,7 +28,7 @@ const BigAudioButton = ({ onPress }) => {
 
 const styles = StyleSheet.create({
   button: {
-    borderRadius: 100,
+    borderRadius: 50,
     alignItems: 'center',
     justifyContent: 'center',
   },
